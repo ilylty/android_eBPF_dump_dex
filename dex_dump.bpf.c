1b2c90c640f67e1bbbe5c41095e59d121c32833d
vluny <maskperson114514@gmail.com>
2026-05-24 13:58:24 +0800

    Remove vmlinux dependency from ebpf build

--- dex_dump.bpf.c
+++ dex_dump.bpf.c
@@ -1,6 +1,12 @@
-#include "vmlinux.h"
+#include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_tracing.h>
+
+struct user_pt_regs {
+    __u64 regs[31];
+    __u64 sp;
+    __u64 pc;
+    __u64 pstate;
+};
 
 struct dex_event {
     __u64 base;
@@ -15,10 +21,10 @@ struct {
 } rb SEC(".maps");
 
 SEC("uprobe/dex_file_open")
-int uprobe_dex_open(struct pt_regs *ctx)
+int uprobe_dex_open(struct user_pt_regs *ctx)
 {
-    __u64 base = PT_REGS_PARM2(ctx);
-    __u64 size = PT_REGS_PARM3(ctx);
+    __u64 base = ctx->regs[1];
+    __u64 size = ctx->regs[2];
     __u32 pid = bpf_get_current_pid_tgid() >> 32;
 
     if (size < 51200 || size > 104857600) {
